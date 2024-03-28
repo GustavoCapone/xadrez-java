f301c7b0e9991a71048e39df236fee588ac63ac3
Gustavo Capone <gustavo.capone.sousa@gmail.com>
2024-03-28 16:00:48 -0300

    Criação de tabuleiro pt1

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,13 +1,15 @@
 package application;
 
+import boardgame.Board;
 import boardgame.position;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		position pos = new position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
+		
+		
 	}
 
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Piece {
+
+	protected position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+
+		this.board = board;
+		position = null;
+		
+	}
+
+
+	protected Board getBoard() {
+		return board;
+	}
+			
+	
+}
